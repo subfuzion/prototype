d4d3a61dffa54931d8daf940f25a3598b93f7265
Tony Pujals <subfuzion@users.noreply.github.com>
2024-04-14 11:16:07 -0700

    convention: add and configure prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,27 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  trailingComma: 'es5',
+
+  plugins: [
+    'prettier-plugin-astro',
+  ],
+
+  overrides: [
+    {
+      editorconfig: true,
+      files: [ ".editorconfig", ".eslintignore" ],
+      options: { "parser": "json" }
+    },
+    {
+      files: '*.astro',
+      options: { parser: 'astro' }
+    }
+  ],
+};
+
+export default config;
