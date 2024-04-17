ce7fbf03d4fda7bb904c7582b55bb711f0f7b9cc
Tony Pujals <subfuzion@users.noreply.github.com>
2024-04-16 19:17:32 -0700

    build: add lint:watch

--- npm.sh
+++ npm.sh
@@ -17,6 +17,10 @@ Commands:
   build              build production bundle
   preview            run preview server using production bundle
 
+  check              run format and lint checks
+  lint:fix            run lint and try to fix automatically
+  lint:w :watch      run lint and watch for changes
+
   clean              remove build artifacts
   clean:all          remove build artifacts and delete node_modules
 
@@ -42,22 +46,26 @@ alias run-script="\npm --loglevel=notice run-script"
 
 alias check="\npm run check"
 alias fix="\npm run fix"
+alias lint:fix="\npm run lint:fix --"
+alias lint:watch="\npm run lint:watch --"
+alias lint:w=lint:watch
 
 alias dev="\npm run dev"
 alias clean="\npm run clean"
 alias clean:all="\npm run clean:all"
+alias clean:a=clean:all
 alias build="\npm --loglevel=notice run astro:build"
 alias preview="\npm --loglevel=notice run astro:preview"
 
 alias jest="\npm run test --"
-alias jest:w="\npm run test:watch --"
 alias jest:watch="\npm run test:watch --"
-alias jest:v="\npm run test:verbose --"
+alias jest:w=jest:watch
 alias jest:verbose="\npm run test:verbose --"
+alias jest:v=jest:verbose
 alias jest:vw="\npm run test:verbose:watch --"
 alias jest:wv="\npm run test:verbose:watch --"
-alias jest:m="\npm run test:manual --"
 alias test:manual="\npm run test:manual --"
+alias jest:m=test:manual
 
 menu
 
@@ -68,22 +76,26 @@ restore() {
 
   unalias check >/dev/null
   unalias fix >/dev/null
+  unalias lint:fix >/dev/null
+  unalias lint:watch >/dev/null
+  unalias lint:w >/dev/null
 
   unalias dev >/dev/null
   unalias clean >/dev/null
   unalias clean:all >/dev/null
+  unalias clean:a >/dev/null
   unalias build >/dev/null
   unalias preview >/dev/null
 
   unalias jest >/dev/null
-  unalias jest:w >/dev/null
   unalias jest:watch >/dev/null
-  unalias jest:v >/dev/null
+  unalias jest:w >/dev/null
   unalias jest:verbose >/dev/null
+  unalias jest:v >/dev/null
   unalias jest:vw >/dev/null
   unalias jest:wv >/dev/null
-  unalias jest:m >/dev/null
   unalias test:manual >/dev/null
+  unalias jest:m >/dev/null
 
   unset -f menu >/dev/null
   unset -f restore >/dev/null
